d1889a1eac15dcf83419ec679876dac2df14b1f6
Alex Djalali <alex.djalali@gmail.com>
2012-12-06 16:30:22 -0800

    updates

--- experiments.py
+++ experiments.py
@@ -1,1 +1,21 @@
+import re
+import csv
+from hpc import hpc
 
+queried_collection = hpc.MongoConnection(hpc.HOST, hpc.PORT).query_collection()
+transcripts = hpc.Corpus(queried_collection).get_transcripts
+
+det_re = r'.+?(([A-Za-z]+?)_DT (([Rr]epublican[s]*)_([A-Z]+?)|([Dd]emocrat[s]*)_([A-Z]+?))) .+?'
+
+def get_speaker(transcript, speech):
+    for person in transcript.people:
+        if speech.speaker_first_name in person.first_name:
+            return speech.speaker_first_name
+        else:
+            return False
+
+if __name__ == '__main__':
+    for transcript in transcripts:
+        for speech in transcript.speeches:
+            print re.findall(det_re, speech.pos_speech)
+        print transcript.program_id + " " + "complete"
