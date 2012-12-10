964920d478c8695fb3f81d1122e663d16c5ea5d9
Alex Djalali <alex.djalali@gmail.com>
2012-12-10 06:05:59 -0800

    updates

--- run_experiments.py
+++ run_experiments.py
@@ -2,8 +2,11 @@ import re
 import csv
 from model.corpus import Corpus
 
+############################################################################
+# CSV File
 CSV = 'csvdump/det_experiment.csv'
 
+# CSV fields
 FIELDS = (
             'program_id',
             'category',
@@ -19,54 +22,64 @@ FIELDS = (
             'full_sentence'
         )
 
+# CSV writer
 WRITER = csv.writer(open(CSV, 'wb'))
 
+# Regular expresisons
 DET_RE = r'((([A-Za-z]+?)_(DT) ([Rr]epublican[s]{0,1}_[A-Z]+?|[Dd]emocrat[s]{0,1}_[A-Z]+?)) .+?\.)'
 
 ############################################################################
 
-def get_speaker(transcript, speech):
+def get_transcript_data(transcript):
+    date_aired = unicode(transcript.date_aired.ctime())
+    row = (
+                transcript.program_id,
+                transcript.category,
+                transcript.location,
+                date_aired,
+        )
+    return row
+
+def get_speaker_data(transcript, speech):
     for person in transcript.people:
         if speech.speaker_first_name in person.first_name and speech.speaker_last_name in person.last_name:
-            return person
+            row = (
+                    person.full_name,
+                    person.party,
+                    person.state,
+                    person.office,
+                )
+            return row
         else:
             return False
 
 ############################################################################
 
 if __name__ == "__main__":
+    c = 0
     WRITER.writerow(FIELDS)
     transcripts = Corpus().get_transcripts
     for transcript in transcripts:
         for speech in transcript.speeches:
             matches = re.findall(DET_RE, speech.pos_speech)
             if matches:
-                speaker = get_speaker(transcript, speech)
                 for match in matches:
+                    syntactic_category = match[3]
+                    determiner = match[2].lower()
+                    data_point = re.match(r'(.+?)_[A-Z]+?', match[4]).group(1).lower()
+                    full_sentence = match[1]
                     try:
-                        date_aired = unicode(transcript.date_aired.month) + ";" + unicode(transcript.date_aired.day) + ";" + unicode(transcript.date_aired.year)
-                        transcript_time = unicode(speech.transcript_time)
-                        syntactic_category = match[3]
-                        determiner = match[2].lower()
-                        data_point = re.match(r'(.+?)_[A-Z]+?', match[4]).group(1).lower()
-                        full_sentence = match[0]
-                        row = (
-                                    transcript.program_id,
-                                    transcript.category,
-                                    transcript.location,
-                                    date_aired,
-                                    speaker.full_name,
-                                    speaker.party,
-                                    speaker.state,
-                                    speaker.office,
+                        row = get_transcript_data(transcript)
+                        row = row + get_speaker_data(transcript, speech)
+                        row = row + (
                                     syntactic_category,
                                     determiner,
                                     data_point,
                                     full_sentence,
                                 )
                         WRITER.writerow(row)
-                        print "Row successfully written to %s" %(CSV,)
+                        c += 1
+                        print "Row #%s successfully written to %s" %(c, CSV,)
                     except:
                         pass
-        print "Transcript #" + transcript.program_id + " successfully written to %s" %(CSV,)
 
