c5921d4c7a6a1bab29a3f2c98e4ed00621ba6eb2
Alex Djalali <alex.djalali@gmail.com>
2012-12-09 19:27:06 -0800

    updates

--- experiments.py
+++ experiments.py
@@ -1,11 +1,19 @@
 import re
-from hpc.corpus import Corpus
+import csv
+from model.corpus import Corpus
 
 ############################################################################
 
-transcripts = Corpus().get_transcripts
+# CSV directory
+CSV = 'csv/determiner_experiment.csv'
 
+# Regular expresisons
 det_re = r'.+?(([A-Za-z]+?)_DT (([Rr]epublican[s]*)_([A-Z]+?)|([Dd]emocrat[s]*)_([A-Z]+?))) .+?'
+
+############################################################################
+
+def to_csv(re, transcript):
+    experiment_writer = csv.writer(open(CSV, 'wb'), delimiter=",", )
 
 def get_speaker(transcript, speech):
     for person in transcript.people:
@@ -14,7 +22,8 @@ def get_speaker(transcript, speech):
         else:
             return False
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    transcripts = Corpus().get_transcripts
     for transcript in transcripts:
         for speech in transcript.speeches:
             print re.findall(det_re, speech.pos_speech)
--- scripts/json_to_mongo.py
+++ scripts/json_to_mongo.py
@@ -2,10 +2,18 @@ import json
 from glob import glob
 from pymongo import Connection
 
-db = Connection(host='localhost', port=27017).cspan
+########################################################################
+
+# Mongo DB connection & collection
+COLLECTION = Connection(host='localhost', port=27017).cspan
+
+# Transcript directory
 DIR = '/Users/ajdjalali/Desktop/cspan/tagged_transcripts/*.json'
+
+########################################################################
+
 for filename in glob(DIR):
     with open(filename) as fp:
         doc = json.load(fp)
 
-    db.house_hearings.save(doc)
+    COLLECTION.house_hearings.save(doc)
